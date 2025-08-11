adb138268726b5864e0ed45bb9ed5f0dea40ef7e
Rudra-solanki <157590645+Solanki-rudra@users.noreply.github.com>
2025-08-11 23:35:19 +0530

    made api to create one time owner and some changes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const db = require('./config/mongooseConnection');
 
 const initialRouter = require('./routes/initialRoute');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', initialRouter)
 
--- routes/ownersRoute.js
+++ routes/ownersRoute.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const ownerModel = require('../models/ownerModel');
 const Router = express.Router();
 
 Router.get('/', (req, res) => {
     res.send('Welcome to the owners API!');
-})
+});
+
+console.log('ENV:', process.env.ENV);
+
+if (process.env.ENV === 'development') {
+    Router.post('/create', async (req, res) => {
+        try {
+            const owners = await ownerModel.find();
+            if (owners.length > 0) {
+                return res.status(400).send('Owner already exists');
+            }
+
+            const { fullname, email, password } = req.body;
+
+            const createdOwner = await ownerModel.create({
+                fullname,
+                email,
+                password
+            });
+
+            res.status(201).send(createdOwner);
+        } catch (err) {
+            res.status(500).send('Error creating owner: ' + err.message);
+        }
+    });
+}
 
 module.exports = Router;
